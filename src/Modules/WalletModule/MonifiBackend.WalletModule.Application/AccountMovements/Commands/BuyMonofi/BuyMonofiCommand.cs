9f91493b7202bb235cd570c04d5f3b9b3ef25e3b
Hakan GÜZEL <hakan-guzel@outlook.com>
2022-07-20 20:52:33 +0300

    feat BuyMonofiCommand coding start

---
+++ src/Modules/WalletModule/MonifiBackend.WalletModule.Application/AccountMovements/Commands/BuyMonofi/BuyMonofiCommand.cs
@@ -0,0 +1,8 @@
+﻿using MonifiBackend.Core.Application.Abstractions;
+
+namespace MonifiBackend.WalletModule.Application.AccountMovements.Commands.BuyMonofi;
+
+public class BuyMonofiCommand : ICommand<BuyMonofiCommandResponse>
+{
+    public string Email { get; set; }
+}
