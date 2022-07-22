a69a7a5f8389433cf1625662f19cd807fc85f31b
Hakan GÜZEL <hakan-guzel@outlook.com>
2022-07-22 15:21:45 +0300

    feat: Mail

--- src/Modules/Core/MonifiBackend.Core.Infrastructure/Notifications/YandexEmailAdapter.cs
+++ src/Modules/Core/MonifiBackend.Core.Infrastructure/Notifications/YandexEmailAdapter.cs
@@ -1,11 +1,11 @@
 ﻿using MailKit.Net.Smtp;
-using MailKit.Security;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Options;
 using MimeKit;
 using MimeKit.Text;
 using MonifiBackend.Core.Domain.Notifications;
 using MonifiBackend.Core.Infrastructure.Environments;
+using System.Security.Authentication;
 
 namespace MonifiBackend.Core.Infrastructure.Notifications;
 
@@ -33,8 +33,11 @@ public class YandexEmailAdapter : IEmailPort
 
         // send email
         using var smtp = new SmtpClient();
-        smtp.Connect(_appSettings.EmailConfigurations.SmtpServer, _appSettings.EmailConfigurations.Port, SecureSocketOptions.StartTls);
+        smtp.SslProtocols = SslProtocols.Ssl3 | SslProtocols.Tls | SslProtocols.Tls11 | SslProtocols.Tls12 | SslProtocols.Tls13;
+
+        smtp.Connect(_appSettings.EmailConfigurations.SmtpServer, _appSettings.EmailConfigurations.Port, true);
         smtp.Authenticate(_appSettings.EmailConfigurations.UserName, _appSettings.EmailConfigurations.Password);
+        smtp.CheckCertificateRevocation = false;
         smtp.Send(email);
         smtp.Disconnect(true);
     }
