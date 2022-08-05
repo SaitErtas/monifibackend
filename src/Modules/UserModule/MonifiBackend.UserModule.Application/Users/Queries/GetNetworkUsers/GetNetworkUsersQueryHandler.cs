27b8401b7f7e4ce721aa200dcf20535468cb6e46
Hakan GÜZEL <hakan-guzel@outlook.com>
2022-08-05 16:45:27 +0300

    feat: Leader, Member and Sub Localize added

--- src/Modules/UserModule/MonifiBackend.UserModule.Application/Users/Queries/GetNetworkUsers/GetNetworkUsersQueryHandler.cs
+++ src/Modules/UserModule/MonifiBackend.UserModule.Application/Users/Queries/GetNetworkUsers/GetNetworkUsersQueryHandler.cs
@@ -1,4 +1,6 @@
-﻿using MonifiBackend.Core.Application.Abstractions;
+﻿using Microsoft.Extensions.Localization;
+using MonifiBackend.Core.Application.Abstractions;
+using MonifiBackend.Core.Infrastructure.Localize;
 using MonifiBackend.UserModule.Domain.Users;
 
 namespace MonifiBackend.UserModule.Application.Users.Queries.GetNetworkUsers;
@@ -6,9 +8,11 @@ namespace MonifiBackend.UserModule.Application.Users.Queries.GetNetworkUsers;
 internal class GetNetworkUsersQueryHandler : IQueryHandler<GetNetworkUsersQuery, GetNetworkUsersQueryResponse>
 {
     private readonly IUserQueryDataPort _userQueryDataPort;
-    public GetNetworkUsersQueryHandler(IUserQueryDataPort userQueryDataPort)
+    private readonly IStringLocalizer<Resource> _stringLocalizer;
+    public GetNetworkUsersQueryHandler(IUserQueryDataPort userQueryDataPort, IStringLocalizer<Resource> stringLocalizer)
     {
         _userQueryDataPort = userQueryDataPort;
+        _stringLocalizer = stringLocalizer;
     }
     public async Task<GetNetworkUsersQueryResponse> Handle(GetNetworkUsersQuery request, CancellationToken cancellationToken)
     {
@@ -18,6 +22,6 @@ internal class GetNetworkUsersQueryHandler : IQueryHandler<GetNetworkUsersQuery,
         networkUserIds.Add(request.UserId);
         var networkUsers = await _userQueryDataPort.GetAllNetworkAsync(networkUserIds);
 
-        return new GetNetworkUsersQueryResponse(user, meFirstNetworkUsers, networkUsers);
+        return new GetNetworkUsersQueryResponse(user, meFirstNetworkUsers, networkUsers, _stringLocalizer);
     }
 }
--- src/Modules/UserModule/MonifiBackend.UserModule.Application/Users/Queries/GetNetworkUsers/GetNetworkUsersQueryResponse.cs
+++ src/Modules/UserModule/MonifiBackend.UserModule.Application/Users/Queries/GetNetworkUsers/GetNetworkUsersQueryResponse.cs
@@ -1,15 +1,17 @@
-﻿using MonifiBackend.Core.Domain.Utility;
+﻿using Microsoft.Extensions.Localization;
+using MonifiBackend.Core.Domain.Utility;
+using MonifiBackend.Core.Infrastructure.Localize;
 using MonifiBackend.UserModule.Domain.Users;
 
 namespace MonifiBackend.UserModule.Application.Users.Queries.GetNetworkUsers;
 
 public class GetNetworkUsersQueryResponse
 {
-    public GetNetworkUsersQueryResponse(User user, List<User> meFirstNetworkUsers, List<User> networkUsers)
+    public GetNetworkUsersQueryResponse(User user, List<User> meFirstNetworkUsers, List<User> networkUsers, IStringLocalizer<Resource> stringLocalizer)
     {
-        var clearMeFirstNetworkUsers = meFirstNetworkUsers.Select(x => new GetUser(x.Email, x.ReferanceCode, x.FullName, "Member",x.Id)).ToList();
-        var clearNetworkUsers = networkUsers.Select(x => new GetUser(x.Email.CapitalizeFirstAndHideText(), x.ReferanceCode, x.FullName.CapitalizeFirstAndHideText(), "Sub",x.Id)).ToList();
-        var clearUser = new GetUser(user.Email, user.ReferanceCode, user.FullName, "Leader",user.Id);
+        var clearMeFirstNetworkUsers = meFirstNetworkUsers.Select(x => new GetUser(x.Email, x.ReferanceCode, x.FullName, $"{stringLocalizer["Member"]}", x.Id)).ToList();
+        var clearNetworkUsers = networkUsers.Select(x => new GetUser(x.Email.CapitalizeFirstAndHideText(), x.ReferanceCode, x.FullName.CapitalizeFirstAndHideText(), $"{stringLocalizer["Sub"]}", x.Id)).ToList();
+        var clearUser = new GetUser(user.Email, user.ReferanceCode, user.FullName, $"{stringLocalizer["Leader"]}", user.Id);
         NetworkUsers.Add(clearUser);
         NetworkUsers.AddRange(clearMeFirstNetworkUsers);
         NetworkUsers.AddRange(clearNetworkUsers);
@@ -20,7 +22,7 @@ public class GetNetworkUsersQueryResponse
 }
 public class GetUser
 {
-    public GetUser(string email, string referenceCode, string fullName, string status,int userId)
+    public GetUser(string email, string referenceCode, string fullName, string status, int userId)
     {
         Email = email;
         ReferenceCode = referenceCode;
