8bfff1d1f8760a76333ac90e411effb025bbeaf1
Hakan GÜZEL <hakan-guzel@outlook.com>
2022-08-24 03:20:16 +0300

    feat: nobonus-purchased-movements endpoint added

---
+++ src/Modules/WalletModule/MonifiBackend.WalletModule.Application/AccountMovements/Queries/GetNoBonusPurchasedMovements/GetNoBonusPurchasedMovementsQuery.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using MonifiBackend.Core.Application.Abstractions;
+
+namespace MonifiBackend.WalletModule.Application.AccountMovements.Queries.GetNoBonusPurchasedMovements;
+
+public class GetNoBonusPurchasedMovementsQuery : IQuery<GetNoBonusPurchasedMovementsQueryResponse>
+{
+    public GetNoBonusPurchasedMovementsQuery(int userId)
+    {
+        UserId = userId;
+    }
+    public int UserId { get; }
+
+}
+internal class GetNoBonusPurchasedMovementsQueryValidator : AbstractValidator<GetNoBonusPurchasedMovementsQuery>
+{
+    public GetNoBonusPurchasedMovementsQueryValidator()
+    {
+        RuleFor(x => x.UserId)
+            .NotEmpty().WithMessage("UserId alanı boş bırakılamaz.");
+
+    }
+}
