d8c699554367eac0d1edee780308c23e6723c3f1
Hakan GÜZEL <hakan-guzel@outlook.com>
2022-09-11 00:16:24 +0300

    feat: Update Package

--- src/Modules/PackageModule/MonifiBackend.PackageModule.Application/Packages/Commands/UpdatePackage/UpdatePackageCommand.cs
+++ src/Modules/PackageModule/MonifiBackend.PackageModule.Application/Packages/Commands/UpdatePackage/UpdatePackageCommand.cs
@@ -10,39 +10,19 @@ public class UpdatePackageCommand : ICommand<UpdatePackageCommandResponse>
 {
     [JsonIgnore]
     public int Id { get; set; }
-    public string Name { get; set; }
     public int MinValue { get; set; }
     public int MaxValue { get; set; }
-    public int ChangePeriodDay { get; set; }
-    public string Icon { get; set; }
-    public int Bonus { get; set; }
-    public List<UpdatePackageDetail> Details { get; set; }
 }
 
-public class UpdatePackageDetail
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public int Duration { get; set; }
-    public int Commission { get; set; }
-}
 internal class UpdatePackageCommandValidator : AbstractValidator<UpdatePackageCommand>
 {
     public UpdatePackageCommandValidator(IStringLocalizer<Resource> stringLocalizer)
     {
         RuleFor(x => x.Id)
             .NotEmpty().WithMessage(x => $"{string.Format(stringLocalizer["FieldRequired"], nameof(x.Id))}");
-        RuleFor(x => x.Name)
-            .NotEmpty().WithMessage(x => $"{string.Format(stringLocalizer["FieldRequired"], nameof(x.Name))}");
         RuleFor(x => x.MinValue)
             .NotEmpty().WithMessage(x => $"{string.Format(stringLocalizer["FieldRequired"], nameof(x.MinValue))}");
         RuleFor(x => x.MaxValue)
             .NotEmpty().WithMessage(x => $"{string.Format(stringLocalizer["FieldRequired"], nameof(x.MaxValue))}");
-        RuleFor(x => x.ChangePeriodDay)
-            .NotEmpty().WithMessage(x => $"{string.Format(stringLocalizer["FieldRequired"], nameof(x.ChangePeriodDay))}");
-        RuleFor(x => x.Icon)
-            .NotEmpty().WithMessage(x => $"{string.Format(stringLocalizer["FieldRequired"], nameof(x.Icon))}");
-        RuleFor(x => x.Bonus)
-            .NotEmpty().WithMessage(x => $"{string.Format(stringLocalizer["FieldRequired"], nameof(x.Bonus))}");
     }
 }
--- src/Modules/PackageModule/MonifiBackend.PackageModule.Application/Packages/Commands/UpdatePackage/UpdatePackageCommandHandler.cs
+++ src/Modules/PackageModule/MonifiBackend.PackageModule.Application/Packages/Commands/UpdatePackage/UpdatePackageCommandHandler.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.Extensions.Localization;
 using MonifiBackend.Core.Application.Abstractions;
-using MonifiBackend.Core.Domain.Base;
 using MonifiBackend.Core.Domain.Exceptions;
 using MonifiBackend.Core.Domain.Utility;
 using MonifiBackend.Core.Infrastructure.Localize;
-using MonifiBackend.PackageDetailModule.Domain.PackageDetails;
 using MonifiBackend.PackageModule.Domain.Packages;
 
 namespace MonifiBackend.PackageModule.Application.Packages.Commands.UpdatePackage;
@@ -27,27 +25,9 @@ internal class UpdatePackageCommandHandler : ICommandHandler<UpdatePackageComman
         var package = await _packageQueryDataPort.GetPackageAsync(request.Id);
         AppRule.ExistsAndActive(package, new BusinessValidationException(BusinessValidationMessageType.NOT_FOUND, nameof(request.Id), request.Id));
 
-        package.SetName(request.Name);
         package.SetMinValue(request.MinValue);
         package.SetMaxValue(request.MaxValue);
-        package.SetChangePeriodDay(request.ChangePeriodDay);
-        package.SetBonus(request.Bonus);
-        package.SetIcon(request.Icon);
 
-        foreach (var detail in request.Details)
-        {
-            var item = package.Details.FirstOrDefault(x => x.Id == detail.Id);
-            if (item != null)
-            {
-                item.SetName(detail.Name);
-                item.SetDuration(detail.Duration);
-                item.SetCommission(detail.Commission);
-            }
-            else
-            {
-                package.AddDetail(PackageDetail.CreateNew(detail.Name, detail.Duration, detail.Commission, BaseStatus.Active));
-            }
-        }
         var status = await _packageCommandDataPort.SaveAsync(package);
         AppRule.True(status, new BusinessValidationException(BusinessValidationMessageType.NOT_UPDATED, nameof(request.Id), request.Id));
 
