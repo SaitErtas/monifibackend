f2b0e985a0b3bba5e7d1f35e6b3be0379238a6e1
SaitErtas <52601084+SaitErtas@users.noreply.github.com>
2022-10-18 22:06:20 +0300

    .

---
+++ src/Modules/UserModule/MonifiBackend.UserModule.Application/Users/Queries/GetOrganizationalChart/GetOrganizationalChartQuery.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
+using MonifiBackend.Core.Application.Abstractions;
+using MonifiBackend.Core.Infrastructure.Localize;
+using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace MonifiBackend.UserModule.Application.Users.Queries.GetOrganizationalCharts
+{
+    public class GetOrganizationalChartQuery : IQuery<GetOrganizationalChartQueryResponse>
+    {
+        public GetOrganizationalChartQuery(int userId)
+        {
+            UserId = userId;
+        }
+        [Required]
+        [SwaggerSchema(Nullable = false)]
+        public int UserId { get; }
+
+    }
+    internal class GetOrganizationQueryValidator : AbstractValidator<GetOrganizationalChartQuery>
+    {
+        public GetOrganizationQueryValidator(IStringLocalizer<Resource> stringLocalizer)
+        {
+   
+
+        }
+    }
+}
---
+++ src/Modules/UserModule/MonifiBackend.UserModule.Application/Users/Queries/GetOrganizationalChart/GetOrganizationalChartQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Localization;
+using MonifiBackend.Core.Application.Abstractions;
+using MonifiBackend.Core.Domain.Exceptions;
+using MonifiBackend.Core.Infrastructure.Localize;
+using MonifiBackend.Core.Domain.Utility;
+using MonifiBackend.UserModule.Domain.Users;
+
+namespace MonifiBackend.UserModule.Application.Users.Queries.GetOrganizationalCharts;
+
+internal class GetOrganizationalChartQueryHandler : IQueryHandler<GetOrganizationalChartQuery, GetOrganizationalChartQueryResponse>
+{
+    private readonly IStringLocalizer<Resource> _stringLocalizer;
+    private readonly IUserQueryDataPort _userQueryDataPort;
+    public GetOrganizationalChartQueryHandler(IUserQueryDataPort userQueryDataPort, IStringLocalizer<Resource> stringLocalizer)
+    {
+        _userQueryDataPort = userQueryDataPort;
+        _stringLocalizer = stringLocalizer;
+    }
+    public async Task<GetOrganizationalChartQueryResponse> Handle(GetOrganizationalChartQuery request, CancellationToken cancellationToken)
+    {
+        var users = await _userQueryDataPort.GetAsync();
+
+
+        return new GetOrganizationalChartQueryResponse(users);
+    }
+}
