6a165c349dbd34f209be987fdae8ff15ea9bfff0
Hakan GÜZEL <hakan-guzel@outlook.com>
2022-10-29 15:56:34 +0300

    feat: Fa2Code added

---
+++ src/Presentation/MonifiBackend.API/Migrations/20221027231958_Fa2Auth.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MonifiBackend.API.Migrations
+{
+    public partial class Fa2Auth : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Fa2Code",
+                table: "Users",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Fa2Code",
+                table: "Users");
+        }
+    }
+}
